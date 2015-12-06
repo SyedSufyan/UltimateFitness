30da0cf9d1efa37b2c0ce6cf1f4d813ff34409b8
Syed Sufyan Sher <ssher@hawk.iit.edu>
2015-12-05 21:46:06 -0600

    sergio this is comming

--- app/src/main/java/com/example/vinayreddy/myapplication/FormActivity.java
+++ app/src/main/java/com/example/vinayreddy/myapplication/FormActivity.java
@@ -13,7 +13,7 @@ public class FormActivity extends AppCompatActivity {
         setContentView(R.layout.activity_form);
 
     }
-
+    //test2
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
