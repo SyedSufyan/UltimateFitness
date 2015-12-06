411ef440940ab759b276cf35c90e1f07c5a8b8e3
sergiogilupm <sergio.gil.luque@gmail.com>
2015-12-05 18:31:29 -0600

    Adding stopwatch blank activity

--- app/src/main/java/com/example/vinayreddy/myapplication/StopWatch.java
+++ app/src/main/java/com/example/vinayreddy/myapplication/StopWatch.java
@@ -1,7 +1,13 @@
 package com.example.vinayreddy.myapplication;
 
-/**
- * Created by sergio gil on 12/5/15.
- */
-public class StopWatch {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Stopwatch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+    }
 }
