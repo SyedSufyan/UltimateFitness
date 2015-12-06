bc3ed6b27fef4c9fea92e5809302edf18342bad9
Syed Sufyan Sher <ssher@hawk.iit.edu>
2015-12-05 21:02:35 -0600

    form and object class

---
+++ app/src/main/java/com/example/vinayreddy/myapplication/Form.java
@@ -0,0 +1,122 @@
+package com.example.vinayreddy.myapplication;
+
+import java.util.Date;
+
+/**
+ * Created by SyedSufyan on 12/5/2015.
+ */
+public class Form {
+
+
+    private  int _uid;
+    private int height;
+    private int weight;
+    private int wrist;
+    private int age;
+    private int gender;
+    private Date date;
+    private int hip;
+    private int forearm;
+    private double rbmi;
+    private double rbmr;
+    private double rbfp;
+
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int get_uid() {
+        return _uid;
+    }
+
+    public void set_uid(int _uid) {
+        this._uid = _uid;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getWrist() {
+        return wrist;
+    }
+
+    public void setWrist(int wrist) {
+        this.wrist = wrist;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getHip() {
+        return hip;
+    }
+
+    public void setHip(int hip) {
+        this.hip = hip;
+    }
+
+    public int getForearm() {
+        return forearm;
+    }
+
+    public void setForearm(int forearm) {
+        this.forearm = forearm;
+    }
+
+    public double getRbmi() {
+        return rbmi;
+    }
+
+    public void setRbmi(double rbmi) {
+        this.rbmi = rbmi;
+    }
+
+    public double getRbmr() {
+        return rbmr;
+    }
+
+    public void setRbmr(double rbmr) {
+        this.rbmr = rbmr;
+    }
+
+    public double getRbfp() {
+        return rbfp;
+    }
+
+    public void setRbfp(double rbfp) {
+        this.rbfp = rbfp;
+    }
+
+
+}
