c9a57d210912a9f4d780527f52fc229989cdfb1f
sergiogilupm <sergio.gil.luque@gmail.com>
2015-12-06 19:05:37 -0600

    Updating Histogram object

--- app/src/main/java/com/cs442/uf/myapplication/Histogram.java
+++ app/src/main/java/com/cs442/uf/myapplication/Histogram.java
@@ -3,7 +3,21 @@ package com.cs442.uf.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Date;
+
 public class Histogram extends AppCompatActivity {
+
+    private String userName;
+    private Date date;
+    private Double height;
+    private Double weight;
+    private int age;
+    private char gender;
+    private double waist;
+    private double wrist;
+    private double forearm;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
